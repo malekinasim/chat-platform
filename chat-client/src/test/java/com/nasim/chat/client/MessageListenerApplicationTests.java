4e96d0a7a3893074f83d5dfd11e4e2de29756fa4
maleki.nasim@gmail.com <maleki.nasim@gmail.com>
2026-07-21 14:26:40 +0200

    change tes add mock bean

--- chat-client/src/test/java/com/nasim/chat/client/MessageListenerApplicationTests.java
+++ chat-client/src/test/java/com/nasim/chat/client/MessageListenerApplicationTests.java
@@ -1,13 +1,22 @@
 package com.nasim.chat.client;
 
+import com.nasim.chat.client.socket.client.ChatClientReceiver;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+
+import java.nio.channels.SocketChannel;
 
 @SpringBootTest
 class MessageListenerApplicationTests {
 
+	@MockitoBean
+	private SocketChannel socketChannel;
+
+	@MockitoBean
+	private ChatClientReceiver chatClientReceiver;
+
 	@Test
 	void contextLoads() {
 	}
-
 }
