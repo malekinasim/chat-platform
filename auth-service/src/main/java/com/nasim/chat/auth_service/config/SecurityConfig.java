1d801351baf00dc4e1cd5935688d5d4652361fe9
maleki.nasim@gmail.com <maleki.nasim@gmail.com>
2026-07-23 15:37:52 +0200

    add oidc config

--- auth-service/src/main/java/com/nasim/chat/auth_service/config/SecurityConfig.java
+++ auth-service/src/main/java/com/nasim/chat/auth_service/config/SecurityConfig.java
@@ -1,13 +1,28 @@
 package com.nasim.chat.auth_service.config;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.SecurityBuilder;
 import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.stereotype.Component;
 
 @Component
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain securityFilterChain( HttpSecurity http){
+       return http.authorizeHttpRequests(
+                athurize->
+                        athurize.requestMatchers("/login/**",
+                                "/oauth2/**").permitAll()
+                                .anyRequest().authenticated()
+
+        ).oauth2Login(Customizer.withDefaults())
+                .build();
+    }
 }
---
+++ auth-service/src/main/java/com/nasim/chat/auth_service/controler/AuthController.java
@@ -0,0 +1,4 @@
+package com.nasim.chat.auth_service.controler;
+
+public class AuthController {
+}
