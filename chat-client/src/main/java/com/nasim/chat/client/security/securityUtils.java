c29a539f475110ea607fc569c22675ca05bc0bc3
maleki.nasim@gmail.com <maleki.nasim@gmail.com>
2026-07-28 10:38:54 +0200

    websocket inbound message security

--- chat-client/src/main/java/com/nasim/chat/client/security/securityUtils.java
+++ chat-client/src/main/java/com/nasim/chat/client/security/securityUtils.java
@@ -1,6 +1,7 @@
 package com.nasim.chat.client.security;
 
 import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
@@ -18,6 +19,17 @@ public class securityUtils {
         }
 
         return principal.getName();
+    }
+    public static Authentication authenticatedUerInfo() {
+        SecurityContext securityContext=SecurityContextHolder.getContext();
+        Authentication principal=securityContext.getAuthentication();
+        if (principal == null) {
+            throw new AuthenticationCredentialsNotFoundException(
+                    "An authenticated WebSocket user is required"
+            );
+        }
+
+        return principal;
     }
     public static String authenticatedUsername(Principal principal) {
         if (principal == null) {
