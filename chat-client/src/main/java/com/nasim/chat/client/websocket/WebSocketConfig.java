4bf64a33aa91b7ee8531ab6059ee33815f5b9833
maleki.nasim@gmail.com <maleki.nasim@gmail.com>
2026-09-07 23:09:02 +0200

    RabbitMQ config

--- chat-client/src/main/java/com/nasim/chat/client/websocket/WebSocketConfig.java
+++ chat-client/src/main/java/com/nasim/chat/client/websocket/WebSocketConfig.java
@@ -4,6 +4,7 @@ import com.nasim.chat.client.websocket.channelInterceptor.OutboundMessageInterce
 import com.nasim.chat.client.websocket.channelInterceptor.RoomSubscriptionAuthorizationInterceptor;
 import com.nasim.chat.client.websocket.channelInterceptor.StompAuthenticationChannelInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.messaging.simp.config.ChannelRegistration;
@@ -15,24 +16,46 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     public static final String ROOM_TOPIC_PREFIX = "/topic/room/";
     public static final String PRIVATE_TOPIC_PREFIX = "/user/queue/private";
+    private  final String rabbitMQHost;
+    private  final int rabbitMQPort;
+    private  final String rabbitMQUsername;
+    private  final String rabbitMQPassword;
     private final StompAuthenticationChannelInterceptor authenticationInterceptor;
     private final RoomSubscriptionAuthorizationInterceptor roomSubscriptionAuthorizationInterceptor;
     private final OutboundMessageInterceptor outboundMessageInterceptor;
     private TaskScheduler messageBrokerTaskScheduler;
-    public WebSocketConfig(StompAuthenticationChannelInterceptor authenticationInterceptor, RoomSubscriptionAuthorizationInterceptor roomSubscriptionAuthorizationInterceptor, OutboundMessageInterceptor outboundMessageInterceptor) {
+
+    public WebSocketConfig(StompAuthenticationChannelInterceptor authenticationInterceptor,
+                           RoomSubscriptionAuthorizationInterceptor roomSubscriptionAuthorizationInterceptor,
+                           OutboundMessageInterceptor outboundMessageInterceptor,
+                           @Value("${chat.broker-relay.host}")
+                           String rabbitMQHost,
+                           @Value("${chat.broker-relay.port}")
+                           int rabbitMQPort,
+                           @Value("${chat.broker-relay.username}")
+                           String rabbitMQUsername,
+                           @Value("${chat.broker-relay.password}")
+                           String rabbitMQPassword
+                           ) {
         this.authenticationInterceptor = authenticationInterceptor;
         this.roomSubscriptionAuthorizationInterceptor = roomSubscriptionAuthorizationInterceptor;
         this.outboundMessageInterceptor = outboundMessageInterceptor;
+        this.rabbitMQHost =rabbitMQHost;
+        this.rabbitMQPort =rabbitMQPort;
+        this.rabbitMQUsername =rabbitMQUsername;
+        this.rabbitMQPassword =rabbitMQPassword;
     }
+
     @Autowired
     public void setMessageBrokerTaskScheduler(
             @Lazy TaskScheduler taskScheduler
     ) {
         this.messageBrokerTaskScheduler = taskScheduler;
     }
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/ws/chat")
@@ -40,16 +63,27 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer  {
     }
 
     @Override
-    public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.setApplicationDestinationPrefixes("/app")
-                .enableSimpleBroker("/topic", "/queue")
-                .setHeartbeatValue(new long[]{10_000, 10_000})
+    public void configureMessageBroker(
+            MessageBrokerRegistry registry
+    ) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableStompBrokerRelay("/topic", "/queue")
+                .setRelayHost(rabbitMQHost)
+                .setRelayPort(rabbitMQPort)
+                .setClientLogin(rabbitMQUsername)
+                .setClientPasscode(rabbitMQPassword)
+                .setSystemLogin(rabbitMQUsername)
+                .setSystemPasscode(rabbitMQPassword)
+                .setSystemHeartbeatReceiveInterval(10_000)
+                .setSystemHeartbeatSendInterval(10_000)
+                .setUserRegistryBroadcast( "/topic/simp-user-registry")
+                .setUserDestinationBroadcast(  "/topic/unresolved-user-destination")
                 .setTaskScheduler(messageBrokerTaskScheduler);
     }
 
     @Override
     public void configureClientInboundChannel(ChannelRegistration registration) {
-       registration.interceptors(authenticationInterceptor,roomSubscriptionAuthorizationInterceptor);
+        registration.interceptors(authenticationInterceptor, roomSubscriptionAuthorizationInterceptor);
     }
 
     @Override
